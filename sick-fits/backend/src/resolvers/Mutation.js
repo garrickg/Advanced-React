572928609bb9cd31cd8e738ef11b6547bfe65aa6
Garrick Gunn <garrickgunn@gmail.com>
2018-10-23 18:52:22 -0700

    making progress through course

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,13 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, context, info) {
+    // TODO: Check if user is authed
 
-module.exports = mutations;
+    const item = await context.db.mutation.createItem({
+      data: { ...args }
+    }, info);
+    
+    return item;
+  }
+};
+
+module.exports = Mutations;
