bcd8b9d68e78028257912fadcced2da6d5aeb7de
Garrick Gunn <garrickgunn@gmail.com>
2018-12-08 10:25:23 -0800

    added snapshot testing

--- sick-fits/frontend/__tests__/item.test.js
+++ sick-fits/frontend/__tests__/item.test.js
@@ -1,4 +1,5 @@
 import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
 
 import ItemComponent from '../components/item';
 
@@ -12,22 +13,26 @@ const testItem = {
 };
 
 describe('<Item />', () => {
-  it('renders and displays properly', () => {
+  it('renders and matches the snapshot', () => {
     const wrapper = shallow(<ItemComponent item={testItem} />);
-    const PriceTag = wrapper.find('PriceTag');
-    expect(PriceTag.children().text()).toBe('$50');
-    expect(wrapper.find('Title a').text()).toBe(testItem.title);
-    const img = wrapper.find('img');
-    expect(img.props().src).toBe(testItem.image);
-    expect(img.props().alt).toBe(testItem.title);
+    expect(toJSON(wrapper)).toMatchSnapshot();
   });
+  // it('renders and displays properly', () => {
+  //   const wrapper = shallow(<ItemComponent item={testItem} />);
+  //   const PriceTag = wrapper.find('PriceTag');
+  //   expect(PriceTag.children().text()).toBe('$50');
+  //   expect(wrapper.find('Title a').text()).toBe(testItem.title);
+  //   const img = wrapper.find('img');
+  //   expect(img.props().src).toBe(testItem.image);
+  //   expect(img.props().alt).toBe(testItem.title);
+  // });
 
-  it('renders out the button properly', () => {
-    const wrapper = shallow(<ItemComponent item={testItem} />);
-    const ButtonList = wrapper.find('.buttonList');
-    expect(ButtonList.children()).toHaveLength(3);
-    expect(ButtonList.find('Link')).toHaveLength(1);
-    expect(ButtonList.find('AddToCart')).toHaveLength(1);
-    expect(ButtonList.find('DeleteItem')).toHaveLength(1);
-  });
+  // it('renders out the button properly', () => {
+  //   const wrapper = shallow(<ItemComponent item={testItem} />);
+  //   const ButtonList = wrapper.find('.buttonList');
+  //   expect(ButtonList.children()).toHaveLength(3);
+  //   expect(ButtonList.find('Link')).toHaveLength(1);
+  //   expect(ButtonList.find('AddToCart')).toHaveLength(1);
+  //   expect(ButtonList.find('DeleteItem')).toHaveLength(1);
+  // });
 });
